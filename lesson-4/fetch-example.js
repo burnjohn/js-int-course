c6512a844ce65b5436e797548b593c4733bf3d32
ivanlapa <sstage44@gmail.com>
2018-04-14 00:43:20 +0300

    Extended 4 lesson

---
+++ lesson-4/fetch-example.js
@@ -0,0 +1,15 @@
+
+fetch('https://pokeapi.co/api/v2/evolution-chain/?limit=1')
+  .then(function(myBlob) {
+    return myBlob.text();
+  })
+  .then(function(response) {
+    console.log('Resp', response);
+
+    var body = document.querySelector('body');
+    var textContainer = document.createElement('p');
+    var text = document.createTextNode(response);
+
+    textContainer.appendChild(text);
+    body.appendChild(textContainer);
+  });
